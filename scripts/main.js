77548f242f5be1a6266097895a9465d9188b3d6f
TettehSolomon <tettehsolomon106@gmail.com>
2024-04-10 15:38:41 +0100

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,83 @@
+let iconCart = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close')
+let body = document.querySelector('body');
+let listProductHTML = document.querySelector('.listProduct');
+
+let listProducts = [];
+
+iconCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+});
+
+closeCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+});
+
+const addDataToHtml = () => {
+    listProductHTML.innerHTML = '';
+    if(listProducts.length > 0){
+        listProducts.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item');
+            newProduct.innerHTML = `
+                <img src="${product.image}" alt="">
+                <h2>${product.name}</h2>
+                <div class="price">$${product.price}</div>
+                <button class="addCart">Add To Cart</button>
+            `;
+            listProductHTML.appendChild(newProduct);
+        })
+    }
+}
+
+listProductHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('addCart')){
+        alert('1');
+    }
+})
+
+const initApp = () => {
+    //get data from json
+    fetch('products.json')
+    .then(response => response.json())    
+    .then(data => {
+        listProducts = data;
+        addDataToHtml();
+    })
+}
+
+initApp();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
